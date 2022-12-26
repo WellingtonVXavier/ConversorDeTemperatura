a096aa16b5e2a3dad809625039bbb026ab0ab191
wellington.xavier <wellington.xavier@financeiro24horas.com>
2022-12-26 18:02:07 -0300

    "criado um novo Projeto"

---
+++ main.js
@@ -0,0 +1,53 @@
+let f;
+let c;
+//let campo = document.querySelector("#Fahrenheit");
+//let celsius = document.querySelector("#inputC").value
+
+console.log(inputC)
+
+function convFahrenheit() {
+    let celsius = document.querySelector("#inputC").value;
+    let campo = document.querySelector("#Fahrenheit");
+    if (celsius === "") {
+        campo.innerHTML = " "
+    }
+    else {
+        f = (celsius * 1.8) + 32
+
+        exibirValor(f, campo)
+    }
+
+}
+
+function exibirValor(valor, campo) {
+
+    limparCampo(campo)
+    campo.append(`${valor}°`)
+}
+
+function convCelsius() {
+    let fahrenheit = document.querySelector("#inputF").value;
+    let campo = document.querySelector("#Celsius");
+    if (fahrenheit === "") {
+        campo.innerHTML = " "
+    }
+    else {
+        c = (fahrenheit - 32) / 1.8;
+        exibirValor(c, campo)
+    }
+}
+
+console.log(convFahrenheit(36));
+console.log(convCelsius(96.8));
+
+// exibirValor(convFahrenheit(), campo)
+
+function limparCampo(campo) {
+    campo.innerHTML = " "
+}
+
+function maskGrau() {
+    let celsius = document.querySelector("#inputC").value;
+    return celsius.replace(/^(\d{1}).*/, "$1°");
+}
+//replace(/^(\d{1}).*/,"$1°");
